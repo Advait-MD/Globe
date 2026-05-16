a03cd9f332b10049dcf6ce9f7f2c88f4b9238da7
Advait-MD <pro.advaitkaushal@gmail.com>
2026-05-16 13:34:14 +0530

    Sign up function working

--- Civ/src/login.jsx
+++ Civ/src/login.jsx
@@ -1,13 +1,60 @@
 import Openpage from "./globe.jsx";
 import { useState } from "react";
 
+//Login Component
+
 export default function Login(){
  const [isSignup, setIsSignup] = useState(false);
+
+ // username and password
+ const[username, setUsername] = useState("");
+ const[password, setPassword] = useState("");
 
  if(isSignup){
     return <Signup />;
  }
-    
+
+ //login function
+
+ async function handleLogin(){
+
+  try{
+
+    const response = await fetch(
+      "http://127.0.0.1:8000/login",
+      {
+        method: "POST",
+
+        headers:{
+          "Content-Type":"application/json"
+        },
+
+        body: JSON.stringify({
+          username,
+          password
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    if(response.ok){
+      alert("Login successful");
+    }
+    else{
+      alert(data.detail);
+    }
+
+  } catch(error){
+
+    console.log(error);
+
+    alert("Server error");
+  }
+}
+    //login rendering
   return(
     <div className="relative h-screen w-screen">
     
@@ -28,19 +75,24 @@ export default function Login(){
       </h1>
 
      <input
-        type="email"
-        placeholder="Email"
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) =>setUsername(e.target.value)}
         className="border p-3 rounded-lg outline-none focus:ring-2 focus:ring-blue-400 text-white placeholder:text-white/70"
        />
 
        <input
         type="password"
         placeholder="Password"
+        value={password}
+        onChange={(e) =>setPassword(e.target.value)}
         className="border p-3 rounded-lg outline-none focus:ring-2 focus:ring-blue-400 text-white placeholder:text-white/70
         "
       />
 
       <button
+      onClick={handleLogin}
         className="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition
         ">
         Log In
@@ -62,7 +114,54 @@ export default function Login(){
   );
 }
 
+//Signup Component
+
 function Signup(){
+
+    const[username, setUsername] = useState("");
+    const[password, setPassword] = useState("");
+
+    //sigup function
+    async function handleSignup(){
+
+  try{
+
+    const response = await fetch(
+      "http://127.0.0.1:8000/signup",
+      {
+        method: "POST",
+
+        headers:{
+          "Content-Type":"application/json"
+        },
+
+        body: JSON.stringify({
+          username,
+          password
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    if(response.ok){
+      alert("Signup successful");
+    }
+    else{
+      alert(data.detail);
+    }
+
+  } catch(error){
+
+    console.log(error);
+
+    alert("Server error");
+  }
+}
+
+// signup rendering
     return(
         <div className="relative h-screen w-screen">
             <Openpage />
@@ -81,23 +180,23 @@ function Signup(){
 
      <input
         placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
         className=" border p-3 rounded-lg outline-none focus:ring-2 focus:ring-blue-400 text-white placeholder:text-white/70
         "
       />
 
-      <input
-        type="email"
-        placeholder="Email"
-        className="border p-3 rounded-lg outline-none focus:ring-2 focus:ring-blue-400 text-white placeholder:text-white/70"
-       />
        <input
         type="password"
         placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         className=" border p-3 rounded-lg outline-none focus:ring-2 focus:ring-blue-400 text-white placeholder:text-white/70
         "
       />
 
       <button
+      onClick={handleSignup}
         className=" bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition
         ">
         Sign Up
